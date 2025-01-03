430c81facad300883ade8f925c2ae8be6570e569
Coffee☕️ <coffee@coffeebot.dev>
2025-01-02 21:56:22 -0500

    simplify

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,9 +5,10 @@ import {Script} from "forge-std/Script.sol";
 import {TimestampAsserter} from "../src/TimestampAsserter.sol";
 
 contract Deploy is Script {
+    
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        new TimestampAsserter{salt: bytes32(0)}();
+        TimestampAsserter timestampAsserter = new TimestampAsserter{salt: bytes32(0)}();
         vm.stopBroadcast();
     }
 }
